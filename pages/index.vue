79d61bc630078b74a29631e032e666d0696ba5d9
Yuki Umeda <yuki.umeda@anycolor.co.jp>
2021-08-16 00:04:29 +0900

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <Tutorial />
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+import { useRoute } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+    const route = useRoute()
+    console.log(route.value)
+  },
+})
+</script>
